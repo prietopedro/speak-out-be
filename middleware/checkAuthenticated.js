5cb40ecdf3db2fd27d15ebcf3879739de2bf334c
Pedro Prieto <pedro.prieto003@gmail.com>
2023-01-04 15:04:29 -0500

    Troubleshooting authentication

--- middleware/checkAuthenticated.js
+++ middleware/checkAuthenticated.js
@@ -2,6 +2,7 @@ module.exports = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     } else {
-        res.status(401).json({ message: 'You are not logged in yet' });
+        console.log(res)
+        res.status(401).json({ message: 'You are not logged in yet', res });
     }
 };
