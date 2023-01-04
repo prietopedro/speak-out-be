21c8b7d8ee1bb438b3ca8b5a72d1b4014e1b9806
Pedro Prieto <pedro.prieto003@gmail.com>
2023-01-04 15:27:46 -0500

    Cookies not setting

--- api/server.js
+++ api/server.js
@@ -33,6 +33,14 @@ server.use(express.json());
 createSession(server);
 initializePassport(passport);
 
+server.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+	next();
+  });
+
 /// LOGIN / SIGNUP / USER ENDPOINTS NEED TO BE REFACTORED INTO IT'S OWN ROUTE
 server.post("/register", (req, res) => {
 	const hashedPassword = bcrypt.hashSync(req.body.password, 10);
